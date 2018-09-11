4a0db3480e296b604a607b885ed82bb8222d5e1b
Léonard Chalvet <leonard@agence-me.com>
2018-09-11 18:29:57 +0200

    Header/Footer include

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer id="footer">
+	<div class="wrapper">
+		<div class="container-logo">
+			<img class="logo" src="img/footer/logo-white.svg">
+			<p>
+				L'Expert Comptable qui vous simplifie vraiment la vie.
+			</p>
+		</div>
+		<div class="container-ul">
+			<ul>
+				<li><a href="">À Propos</a></li>
+				<li><a href="">L’équipe</a></li>
+				<li><a href="">Tarifs</a></li>
+				<li><a href="">Création d’entreprise</a></li>
+				<li><a href="">API</a></li>
+				<li><a href="">Inscription</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Jobs</a></li>
+			</ul>
+			<ul>
+				<li><a href="">Guide des statuts juridiques</a></li>
+				<li><a href="">Guide de la création d’entreprise</a></li>
+				<li><a href="">Guide des kinésithérapeutes</a></li>
+				<li><a href="">Guide des infirmières libérales</a></li>
+			</ul>
+			<ul>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Petites entreprises</a></li>
+				<li><a href="">Professions libérales</a></li>
+				<li><a href="">Création entreprise</a></li>
+			</ul>
+			<ul>
+				<li><a href="">Outils Utile</a></li>
+				<li><a href="">Obtenir votre numéro de TVA </a></li>
+				<li><a href="">intracommunautaire</a></li>
+			</ul>
+			<ul>
+				<li><a href="">Légal</a></li>
+				<li><a href="">Conditions générales d’utilisation</a></li>
+				<li><a href="">Mentions légales</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="foot">
+		<div class="container-left">
+			<p>
+				© 2018 Dougs Compta. Cabinet d'expertise-comptable enregistré à l’Ordre. Tous droits réservés.
+			</p>
+			<div class="li-rs">
+				<a href="">
+					<img src="img/footer/icn-twitter.svg">
+				</a>
+				<a href="">
+					<img src="img/footer/icn-facebook.svg">
+				</a>
+			</div>
+		</div>
+		<div class="container-right">
+			<img src="img/footer/dougs-expert-comptable-en-ligne-pas-cher@2x.png">
+		</div>
+	</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,46 @@
+<header id="header-desktop">
+	<div class="wrapper">
+		<div class="container container-left">
+			<div class="logo">
+				<img src="img/header/logo-black.svg">
+			</div>
+			<div class="sep"></div>
+			<div class="tel">
+				<img src="img/header/icn-tel.svg">
+				<a class="num" href="tel:0428296262">
+					04 28 29 62 62
+				</a>
+			</div>
+		</div>
+		<div class="container container-right">
+			<div class="li-link">
+				<a href="">
+					Fonctionnalités
+				</a>
+				<a href="">
+					Tarifs
+				</a>
+				<a href="">
+					Création d’entreprise
+				</a>
+				<a href="">
+					L’équipe
+				</a>
+				<a href="">
+					Blog
+				</a>
+			</div>
+			<div class="sep"></div>
+			<div class="li-action">
+				<a class="action-signin" href="">
+					Se Connecter
+				</a>
+				<a class="action-signup btn" href="">
+					<div class="btn-content">
+						S’inscrire
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</header>
