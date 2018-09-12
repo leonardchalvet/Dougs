9e9320240526d37820cbc8ace914bbce7ecbc7f3
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-12 17:50:05 +0200

    Deux premières section page creation

---
+++ creation.php
@@ -0,0 +1,107 @@
+<html>
+	<head>
+
+		<title>Creation</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/creation.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('header.php'); ?>
+
+		<main>
+			
+			<section id="section-cover">
+				<div class="wrapper">
+					<img class="pattern" src="img/creation/cover-pattern.svg">
+					<div class="container-text">
+						<h1>
+							Créez votre entreprise rapidement et sereinement, en ligne
+						</h1>
+						<p>
+							Un juriste vous conseille et rédige vos statuts juridiques pour lancer votre activité. <span>Pack création 150€</span>
+						</p>
+						<div class="container-action">
+							<a href="#" class="btn">
+								<div class="btn-content">
+									Créer mon entreprise pour 150€ !
+								</div>
+							</a>
+						</div>
+					</div>	
+				</div>
+			</section>
+
+			<section id="section-jpd">
+				<div class="wrapper">
+					<div class="el el-1">
+						<div class="container-text">
+							<h2 class="creationCommon-section-title">
+								La création de votre entreprise par un juriste professionnel
+							</h2>
+							<ul>
+								<li>
+									<img src="img/creation/jpd-el1.svg">
+									<p>
+										Un juriste professionnel Dougs vous conseille par téléphone pour déterminer la meilleure forme juridique pour votre entreprise.
+									</p>
+								</li>
+								<li>
+									<img src="img/creation/jpd-el2.svg">
+									<p>
+										Le juriste rédige vos statuts juridiques adaptés à votre situation et prend en charge toutes les démarches administratives.
+									</p>
+								</li>
+								<li>
+									<img src="img/creation/jpd-el3.svg">
+									<p>
+										Une fois votre entreprise créée, Dougs devient votre expert-comptable et s'occupe de toutes vos démarches fiscales. Nous mettons tout en oeuvre pour la réussite de votre projet.
+									</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="el el-2">
+						<img src="img/creation/jpd-illu.png">
+					</div>
+					<div class="el el-3">
+						<div class="container-text">
+							<p class="text">
+								Le choix de votre forme juridique est déterminant pour la réussite de votre entreprise. Faire le bon choix peut vous faire gagner jusqu'à 30% sur votre résultat. Une erreur vous ferait travailler plus pour l'administration fiscale que pour vous !
+							</p>
+							<p class="name">
+								Guillaume Paour
+							</p>
+							<span>
+								Expert-comptable chez Dougs
+							</span>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-xpr">
+
+			</section>
+
+			<section id="section-step">
+
+			</section>
+
+		</main>
+
+		<?php include('footer.php'); ?>
+
+		<script type="text/javascript" src="js/index.js"></script>
+	</body>
+</html>
