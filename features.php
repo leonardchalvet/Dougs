142194e300138a3cf0efab373c962e71bca37e52
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-12 14:44:02 +0200

    Page features

---
+++ features.php
@@ -0,0 +1,168 @@
+<html>
+	<head>
+
+		<title>Features</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/features.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('header.php'); ?>
+
+		<main>
+			
+			<section id="section-cover">
+				<div class="wrapper">
+					
+					<div class="container-text">
+						<h1>
+							Le suivi d’un expert-comptable, une application en plus
+						</h1>
+						<p>
+							Dougs est un cabinet d'expertise-comptable moderne, en ligne, pour les consultants, les artisans, le e-commerce, les agences et les freelances
+						</p>
+						<div class="container-action">
+							<a href="#" class="btn">
+								<div class="btn-content">
+									Essai gratuit
+								</div>
+							</a>
+						</div>
+					</div>	
+					<div class="container-img">
+						<img src="img/features/section-cover-img.png">
+					</div>
+				</div>
+			</section>
+
+			<section id="section-app">
+				<div class="wrapper">
+
+					<div class="container-img">
+						<img src="img/features/app-illu.png">
+						<div class="container-tag">
+							<div class="tag tag-1">
+								<img src="img/features/app-el1.svg">
+							</div>
+							<div class="tag tag-2">
+								<img src="img/features/app-el2.svg">
+							</div>
+							<div class="tag tag-3">
+								<img src="img/features/app-el3.svg">
+							</div>
+							<div class="tag tag-4">
+								#OPERATIONS
+							</div>
+							<div class="tag tag-5">
+								#AUTOMATIQUE
+							</div>
+							<div class="tag tag-6">
+								#SYNCHRONISATION
+							</div>
+						</div>
+					</div>
+
+					<div class="container-text">
+						<h2 class="featuresCommon-section-title">
+							La récupération des opérations de votre compte bancaire professionnel est automatique
+						</h2>
+						<p>
+							Pour réaliser votre bilan, nos comptables doivent savoir à quoi correspond chaque opération de votre compte bancaire professionnel. La synchronisation avec votre banque est directe, vous gardez vos relevés de compte chez vous. Les opérations sont catégorisées automatiquement dans Dougs. Vous ne passez que quelques minutes par mois sur votre compta.
+						</p>
+						<a class="btn" href="">
+							<span class="btn-content">Essayez Dougs gratuitement</span>
+						</a>
+					</div>
+
+				</div>
+			</section>
+
+			<section id="section-ntf">
+				<div class="wrapper">
+
+					<div class="container-text">
+						<h2 class="featuresCommon-section-title">
+							Reposez-vous sur nous
+						</h2>
+						<p>
+							Vous recevez des notifications et des alertes à chaque échéance fiscale ou juridique. Toutes vos déclarations sont faites dans les temps, ou même en avance
+						</p>
+					</div>
+
+					<div class="container-img">
+						<img class="pattern" src="img/features/ntf-pattern.svg">
+						<img src="img/features/ntf-illu.png">
+					</div>
+					
+				</div>
+			</section>
+			
+			<section id="section-lpt">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="container-illu">
+								<img src="img/features/lpt-el1-illu.png">
+							</div>
+							<div class="container-text">
+								<h2 class="featuresCommon-section-title">
+									Gérez vos notes de frais… vraiment.
+								</h2>
+								<p>	
+									Lorsque vous faites des notes de frais, votre compte courant d’associé est immédiatement mis à jour. Son solde correspond à la somme que votre entreprise vous doit. Vos notes de frais se saisissent en quelques clics, et vous savez toujours combien vous rembourser.
+								</p>
+							</div>
+						</div>
+						<div class="el">
+							<div class="container-illu">
+								<img src="img/features/lpt-el2-illu.png">
+							</div>
+							<div class="container-text">
+								<h2 class="featuresCommon-section-title">
+									Prennez le contrôle de votre entreprise
+								</h2>
+								<p>	
+									Suivez en un clin d’oeil les chiffres clés de votre entreprise. Des projections de chiffre d’affaires et de résultat vous aide à anticiper. Nous organisons vos chiffres pour vous présenter des données claires et utilisables. Si vous avez une question ou besoin d’un conseil sur une optimisation, vous nous contactez directement. C’est à cela que sert un expert-comptable !
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-bln">
+				<div class="wrapper">
+
+					<div class="container-img">
+						<img src="img/features/bln-illu.png">
+					</div>
+
+					<div class="container-text">
+						<h2 class="featuresCommon-section-title">
+							Votre bilan est préparé, attesté, et transmis à l’administration fiscale par nos comptables Dougs
+						</h2>
+						<p>
+							Chaque année, un comptable Dougs passe plusieurs heures à préparer votre bilan pour attester de la cohérence de vos comptes. Vous échangez avec lui sur les meilleures options à choisir pour votre entreprise et notamment comment optimiser votre rémunération. Quand tout est prêt, nous transmettons votre bilan et liasse fiscale directement à l’administration fiscale pour vous.
+						</p>
+					</div>
+					
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('footer.php'); ?>
+
+		<script type="text/javascript" src="js/index.js"></script>
+	</body>
+</html>
