6424c528cece5e11cf6b0f39f376ba21c282b89f
Léonard Chalvet <leonard@agence-me.com>
2018-09-12 16:59:18 +0200

    Add page TEAM

---
+++ team.php
@@ -0,0 +1,435 @@
+<html>
+	<head>
+
+		<title>Team</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/team.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('header.php'); ?>
+
+		<main>
+
+			<section id="section-cover">
+				<div class="wrapper">
+					<img class="pattern" src="img/team/cover-pattern.svg">
+					<div class="container-text">
+						<h1>Les humains qui font Dougs.</h1>
+						<p>Voici l'équipe qui vous simplifie la vie et vous aide à oublier la paperasse.</p>
+						<a href="" class="btn">
+							<div class="btn-content">
+								► Voir la vidéo
+							</div>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section id="section-team">
+				
+				<div class="wrapper">
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-1.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-1.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-1.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-1.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-3.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-2.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-5.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-6.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="pp" style="background-image: url(img/team/pp-4.jpg);"></div>
+						<div class="container-text">
+							<div class="text">
+								<div class="name">Name</div>
+								<div class="job">Job</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+			<section id="section-fondateur">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="container-illu">
+								<img class="illu" src="img/team/fondateur.png">
+							</div>
+							<div class="container-text">
+								<h2 class="title">
+									Oubliez la saisie manuelle de vos opérations
+								</h2>
+								<p>
+									On nous demande souvent pourquoi nous avons commencé l'aventure Dougs. C’est une bonne question parce qu’il existe déjà des solutions d’expertise-comptable, classiques ou en ligne. Le problème, c’est que nous pensons qu’aucune ne répond parfaitement à nos critères.<br>
+									<br>
+									Nous nous sommes donc rassemblés en une petite équipe soudée, qui communique et qui s’entend bien, pour construire une solution qui corresponde à nos valeurs :
+								</p>
+								<div class="li-el">
+									<div class="el">
+										<img src="img/home/btp-shape1.svg">
+										<p class="text">
+											Aucune saisie manuelle.
+										</p>
+									</div>
+									<div class="el">
+										<img src="img/home/btp-shape1.svg">
+										<p class="text">
+											Les tarifs sont directement affichés sur le site.
+										</p>
+									</div>
+									<div class="el">
+										<img src="img/home/btp-shape1.svg">
+										<p class="text">
+											L’inscription se fait en quelques minutes seulement, sans envoi de dossier papier.
+										</p>
+									</div>
+									<div class="el">
+										<img src="img/home/btp-shape1.svg">
+										<p class="text">
+											Un support client exceptionnel.
+										</p>
+									</div>
+									<div class="el">
+										<img src="img/home/btp-shape1.svg">
+										<p class="text">
+											Sans engagement.
+										</p>
+									</div>
+								</div>
+								<p>
+									Le résultat ? Il dépasse nos espérances : des clients satisfaits qui passent seulement quelques minutes par mois à faire leur comptabilité !<br>
+									<br>
+									Nos clients choisissent Dougs parce que c’est la solution qui leur convient, mais aussi parce que nous faisons Dougs pour les bonnes raisons.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('footer.php'); ?>
+
+		<script type="text/javascript" src="js/index.js"></script>
+	</body>
+</html>
