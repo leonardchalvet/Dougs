d1d88e5ca656aef296e8e6588285f27310eaca82
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-13 10:54:25 +0200

    Deux nouvelles sections pour la page creation

--- creation.php
+++ creation.php
@@ -91,7 +91,46 @@
 			</section>
 
 			<section id="section-xpr">
-
+				<div class="wrapper">
+					<div class="el el-1">
+						<div class="container-text">
+							<h2 class="creationCommon-section-title">
+								Dougs est un cabinet d'expertise-comptable en ligne, c'est une garantie de sérieux
+							</h2>
+							<ul>
+								<li>
+									<img src="img/creation/xpr-icon.svg">
+									<p>
+										En tant que cabinet d'expertise-comptable inscrit à l'Ordre des experts-comptables de Lyon, Dougs se doit d'un haut niveau de qualité de conseil et de professionnalisme. Cela vous garantit que les solutions trouvées pour la création de votre entreprise seront les meilleures pour vous et que le travail sera mené à bien.
+									</p>
+								</li>
+								<li>
+									<img src="img/creation/xpr-icon.svg">
+									<p>
+										Une fois votre entreprise créée, Dougs pourra vous accompagner pour votre comptabilité et votre gestion avec des conseils spécifiques, dédiés aux nouveaux entrepreneurs. Cela vous permettra d'assurer le succès de votre nouvelle activité !
+									</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="el el-2">
+						<div class="container-text">
+							<h2 class="creationCommon-section-title">
+								Découvrez pourquoi Dougs est si populaire pour la création d'entreprise !
+							</h2>
+							<p>
+								Vous souhaitez en savoir plus ? Demandez et un juriste expert en création d'entreprise vous appelle dans les 30 secondes.
+							</p>
+							<div class="container-action">
+								<a href="#" class="btn">
+									<div class="btn-content">
+										Demander un rappel gratuit !
+									</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 
 			<section id="section-step">
@@ -99,7 +138,46 @@
 			</section>
 
 			<section id="section-start">
-
+				<div class="wrapper">
+					<img class="pattern" src="img/creation/start-pattern.svg">
+					<div class="container-title">
+						<h2 class="creationCommon-section-title">
+							Lancez votre activité dés aujourd’hui !
+						</h2>
+					</div>
+					<div class="container-grp">
+						<div class="el el-1">
+							<img class="icon" src="img/creation/start-el1-icon">
+							<p>
+								Commencer à renseigner vos informations sur Dougs, un juriste expert en création d'entreprise vous appelle dans la journée. C'est gratuit, rapide et sans engagement.
+							</p>
+							<div class="container-action">
+								<a href="#" class="btn">
+									<div class="btn-content">
+										Commencer la création de mon compte
+									</div>
+								</a>
+							</div>
+						</div>
+						<div class="el el-2">
+							<div class="circle">OU</div>
+						</div>
+						<div class="el el-3">
+							<img class="icon" src="img/creation/start-el3-icon">
+							<p>
+								 Demandez une présentation de Dougs, un juriste expert en création d’entreprise vous appelle dans la minute. C’est gratuit, rapide et sans engagement 
+							</p>
+							<div class="container-action">
+								<a href="#" class="btn">
+									<div class="btn-content">
+										Appelez-moi
+									</div>
+								</a>
+								<span class="btn-under">Ou appelez le 04.28.29.62.62</span>
+							</div>
+						</div>
+					</div>
+				</div>
 			</section>
 
 		</main>
