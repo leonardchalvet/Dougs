e9ac4ed06d736f13f927488b1afba83154402f03
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-13 17:10:13 +0200

    La dernière section de la page pricing

--- pricing.php
+++ pricing.php
@@ -21,16 +21,52 @@
 
 		<main>
 			
-			<section class="section-price">
-				
+			<section id="section-price">
+				<div class="wrapper">
+					
+				</div>
 			</section>
 			
-			<section class="section-details">
-				
+			<section id="section-details">
+				<div class="wrapper">
+					
+				</div>
 			</section>
 
-			<section class="section-frq">
-				
+			<section id="section-frq">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2 class="pricingCommon-section-title">
+							Questions fréquentes
+						</h2>
+					</div>
+					<div class="container-text">
+						<div class="el el1">
+							<h4>
+								Je ne sais pas quel pack choisir, pouvez-vous m'aider ?
+							</h4>
+							<p>
+								Si vous êtes assujetti à la TVA et/ou en société, vous devez opter pour le pack Liberté à 69€ HT. Si vous êtes profession libérale ou BNC, le pack Essentiel à 49€ HT est fait pour vous. Dans tous les cas sentez-vous libre de nous contacter ou de nous téléphoner au 04.28.29.62.62, nous vous aiderons à prendre une décision.
+							</p>
+						</div>
+						<div class="el el2">
+							<h4>
+								Est-ce réellement sans engagement ?
+							</h4>
+							<p>
+								Oui bien sûr ! Vous pouvez arrêter votre souscription du jour au lendemain. Dans ce cas, vous ne payez que le mois commencé. De plus, quel que soit le pack choisi, les 30 premiers jours vous sont offerts.
+							</p>
+						</div>
+						<div class="el el3">
+							<h4>
+								Dois-je payer un comptable en plus ?
+							</h4>
+							<p>
+								Non, tout est inclus. Dougs est un cabinet d'expertise comptable inscrit à l'ordre des experts comptables. Nous vous fournissons un service complet, sans frais supplémentaire. Nous vous accompagnons aussi dans l'ensemble des démarches liées à votre arrivée chez nous.
+							</p>
+						</div>
+					</div>
+				</div>
 			</section>
 
 		</main>
