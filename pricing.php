6f36fba87db7d301abbdf4aeca02263949e3298e
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-09-13 16:50:15 +0200

    Mise en place page pricing

---
+++ pricing.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+
+		<title>Pricing</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/pricing.css">
+
+		<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('header.php'); ?>
+
+		<main>
+			
+			<section class="section-price">
+				
+			</section>
+			
+			<section class="section-details">
+				
+			</section>
+
+			<section class="section-frq">
+				
+			</section>
+
+		</main>
+
+		<?php include('common-sectionBnd.php'); ?>
+
+		<?php include('footer.php'); ?>
+
+		<script type="text/javascript" src="js/index.js"></script>
+	</body>
+</html>
